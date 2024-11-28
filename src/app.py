d61ac750d3045872cb414b415880784f09ccea05
Shai Goldman <shaitzvi@gmail.com>
2024-11-28 11:24:19 -0500

    black

--- src/app.py
+++ src/app.py
@@ -4,133 +4,129 @@ from rhymer import TextCollection, Lexicon, Rhymer, HebrewC, Loc
 
 NUM_RHYMES = 30
 
+
 def Verse(loc: Loc, text: HebrewC) -> pn.Column:
-    return pn.pane.Markdown(f"{text.__str__()} {loc}")
+    return pn.pane.Markdown(f"{text.__str__()} {loc}")
+
 
 def MyCard(index: int, title: HebrewC, verses: list[Loc]) -> pn.Column:
 
-    verses_items = [
-        Verse(*verses[0]),
-        *[
-            item
-            for i in range(1, len(verses))
-            for item in (pn.layout.Divider(), Verse(*verses[i]))
-        ],
-    ]
-    card_kwargs = dict(
-        title=f"{index}. {title.__str__()}",
-        sizing_mode="stretch_width",
-        styles=dict(margin="15px 30px"),
-    )
+    verses_items = [
+        Verse(*verses[0]),
+        *[
+            item
+            for i in range(1, len(verses))
+            for item in (pn.layout.Divider(), Verse(*verses[i]))
+        ],
+    ]
+    card_kwargs = dict(
+        title=f"{index}. {title.__str__()}",
+        sizing_mode="stretch_width",
+        styles=dict(margin="15px 30px"),
+    )
 
-    if len(verses) < 6:
-        items = verses_items
-    else:
-        items = [pn.Feed(*verses_items, sizing_mode="stretch_both", height=None)]
-        card_kwargs["height"] = 600
+    if len(verses) < 6:
+        items = verses_items
+    else:
+        items = [pn.Feed(*verses_items, sizing_mode="stretch_both", height=None)]
+        card_kwargs["height"] = 600
 
-    return pn.Column(
-        pn.Card(
-            *items,
-            **card_kwargs,
-            collapsed=True,
-        )
-    )
+    return pn.Column(pn.Card(*items, **card_kwargs, collapsed=True,))
+
 
 class App:
 
-    layout: pn.Column
-    input: pn.widgets.TextInput
-    cards_feed: pn.Feed
-    card_holder: pn.Card
-    rhymer: Rhymer
-    spinner: pn.indicators.LoadingSpinner
-    rhymes: list[tuple[HebrewC, list[Loc]]]
+    layout: pn.Column
+    input: pn.widgets.TextInput
+    cards_feed: pn.Feed
+    card_holder: pn.Card
+    rhymer: Rhymer
+    spinner: pn.indicators.LoadingSpinner
+    rhymes: list[tuple[HebrewC, list[Loc]]]
 
-    def __init__(self):
-        self.layout = self.panel_app()
-        texts = TextCollection("bib", "../data")
-        lex = Lexicon(texts)
-        self.rhymer = Rhymer(lex)
-        self.rhymes = []
-        self.spinner = pn.indicators.LoadingSpinner(
-            value=True, size=20, name="Loading..."
-        )
+    def __init__(self):
+        self.layout = self.panel_app()
+        texts = TextCollection("bib", "../data")
+        lex = Lexicon(texts)
+        self.rhymer = Rhymer(lex)
+        self.rhymes = []
+        self.spinner = pn.indicators.LoadingSpinner(
+            value=True, size=20, name="Loading..."
+        )
 
-    def create_cards(self, begin, end):
-        return [
-            MyCard(i + 1, word, verses)
-            for i, (word, verses) in zip(range(begin, end), self.rhymes[begin:end])
-        ] + [self.spinner]
+    def create_cards(self, begin, end):
+        return [
+            MyCard(i + 1, word, verses)
+            for i, (word, verses) in zip(range(begin, end), self.rhymes[begin:end])
+        ] + [self.spinner]
 
-    def update_input(self, event):
-        """
-        Update the card with the input value
-        """
-        self.cards_feed = self._create_feed()
-        self.card_holder.objects = [self.spinner]
-        self.rhymes = list(self.rhymer.rhymes_verses(self.input.value))
-        self.card_holder.title = f"Output: {len(self.rhymes)} Rhymes Found"
-        self.cards_feed.objects = self.create_cards(0, NUM_RHYMES)
-        self.card_holder.objects = [self.cards_feed]
+    def update_input(self, event):
+        """
+        Update the card with the input value
+        """
+        self.cards_feed = self._create_feed()
+        self.card_holder.objects = [self.spinner]
+        self.rhymes = list(self.rhymer.rhymes_verses(self.input.value))
+        self.card_holder.title = f"Output: {len(self.rhymes)} Rhymes Found"
+        self.cards_feed.objects = self.create_cards(0, NUM_RHYMES)
+        self.card_holder.objects = [self.cards_feed]
 
-    def update_scroll(self, event):
-        if self.cards_feed.visible_range is None:
-            print("none event")
-            return
-        _, end = self.cards_feed.visible_range
-        num_cards_curr = len(self.cards_feed)
-        if end > num_cards_curr - (NUM_RHYMES / 3):
-            new_cards = self.create_cards(num_cards_curr, num_cards_curr + NUM_RHYMES)
-            self.cards_feed.pop(-1)
-            self.cards_feed.extend(new_cards)
+    def update_scroll(self, event):
+        if self.cards_feed.visible_range is None:
+            print("none event")
+            return
+        _, end = self.cards_feed.visible_range
+        num_cards_curr = len(self.cards_feed)
+        if end > num_cards_curr - (NUM_RHYMES / 3):
+            new_cards = self.create_cards(num_cards_curr, num_cards_curr + NUM_RHYMES)
+            self.cards_feed.pop(-1)
+            self.cards_feed.extend(new_cards)
 
-    def _create_feed(self):
-        feed = pn.Feed(
-            scroll_button_threshold=20,
-            height=500,
-            styles=dict(padding="15px"),
-        )
-        feed.param.watch(self.update_scroll, "visible_range")
-        return feed
+    def _create_feed(self):
+        feed = pn.Feed(
+            scroll_button_threshold=20, height=500, styles=dict(padding="15px"),
+        )
+        feed.param.watch(self.update_scroll, "visible_range")
+        return feed
 
-    def _create_input(self):
-        input = pn.widgets.TextInput(
-            name="Text Input", placeholder="Enter a string here..."
-        )
-        input.param.watch(self.update_input, "value")
-        return input
+    def _create_input(self):
+        input = pn.widgets.TextInput(
+            name="Text Input", placeholder="Enter a string here..."
+        )
+        input.param.watch(self.update_input, "value")
+        return input
 
-    def panel_app(self) -> pn.Column:
-        """ """
+    def panel_app(self) -> pn.Column:
+        """ """
 
-        self.input = self._create_input()
+        self.input = self._create_input()
 
-        self.cards_feed = self._create_feed
+        self.cards_feed = self._create_feed
 
-        self.card_holder = pn.Card(
-            self.cards_feed,
-            title="Output",
-            collapsible=False,
-            sizing_mode="stretch_width",
-            styles=dict(margin="10px", text_align="center"),
-        )
+        self.card_holder = pn.Card(
+            self.cards_feed,
+            title="Output",
+            collapsible=False,
+            sizing_mode="stretch_width",
+            styles=dict(margin="10px", text_align="center"),
+        )
 
-        return pn.Column(
-            pn.pane.Markdown("# Home Page"),
-            self.input,
-            self.card_holder,
-            styles=dict(direction="rtl"),
-        )
+        return pn.Column(
+            pn.pane.Markdown("# Home Page"),
+            self.input,
+            self.card_holder,
+            styles=dict(direction="rtl"),
+        )
+
 
 if __name__ == "__main__":
-    pn.serve(
-        panels=lambda: App().layout,
-        port=8868,
-        title="App",
-        show=False,
-        autoreload=True,
-        session_history=1,
-        user_key="sub",
-        allow_websocket_origin=["*"],
-    )
+    pn.serve(
+        panels=lambda: App().layout,
+        port=8868,
+        title="App",
+        show=False,
+        autoreload=True,
+        session_history=1,
+        user_key="sub",
+        allow_websocket_origin=["*"],
+    )
--- src/rhymer.py
+++ src/rhymer.py
@@ -5,218 +5,224 @@ import re
 import json
 from hebrew import Hebrew
 
+
 class HebrewC:
-    text: Hebrew
-    DAGESH = "ּ"
+    text: Hebrew
+    DAGESH = "ּ"
 
-    def __init__(self, text: str | Hebrew):
-        if isinstance(text, str):
-            text = Hebrew(text)
-        elif isinstance(text, HebrewC):
-            text = text.text
-        self.text = self.strip(text)
+    def __init__(self, text: str | Hebrew):
+        if isinstance(text, str):
+            text = Hebrew(text)
+        elif isinstance(text, HebrewC):
+            text = text.text
+        self.text = self.strip(text)
 
-    @staticmethod
-    def strip(text: Hebrew) -> Hebrew:
-        return Hebrew(
-            text.no_taamim().no_sof_passuk().__str__().replace("]", "").replace("[", "")
-        )
+    @staticmethod
+    def strip(text: Hebrew) -> Hebrew:
+        return Hebrew(
+            text.no_taamim().no_sof_passuk().__str__().replace("]", "").replace("[", "")
+        )
 
-    @staticmethod
-    def remove_dagesh(text: str | Hebrew) -> str:
-        if isinstance(text, Hebrew):
-            text = text.__str__()
-        clean_text = ""
-        for letter in text:
-            if letter == HebrewC.DAGESH:
-                continue
-            clean_text += letter
+    @staticmethod
+    def remove_dagesh(text: str | Hebrew) -> str:
+        if isinstance(text, Hebrew):
+            text = text.__str__()
+        clean_text = ""
+        for letter in text:
+            if letter == HebrewC.DAGESH:
+                continue
+            clean_text += letter
 
-        return Hebrew(clean_text)
+        return Hebrew(clean_text)
 
-    def words(self):
-        for word in re.split("־| ", self.text.__str__()):
-            yield word
+    def words(self):
+        for word in re.split("־| ", self.text.__str__()):
+            yield word
 
-    def endswith(self, ending: str):
-        clean_self = self.remove_dagesh(self.text)
-        clean_ending = self.remove_dagesh(ending).__str__()
-        return clean_self.endswith(clean_ending)
+    def endswith(self, ending: str):
+        clean_self = self.remove_dagesh(self.text)
+        clean_ending = self.remove_dagesh(ending).__str__()
+        return clean_self.endswith(clean_ending)
 
-    def _bolded_word_iter(self, word: str):
-        if not isinstance(word, str):
-            word = word.__str__()
-        for w in self.words():
-            if w == word:
-                yield f"**{w}**"
-            else:
-                yield w
+    def _bolded_word_iter(self, word: str):
+        if not isinstance(word, str):
+            word = word.__str__()
+        for w in self.words():
+            if w == word:
+                yield f"**{w}**"
+            else:
+                yield w
 
-    def bolded_word(self, word: str):
-        return " ".join(self._bolded_word_iter(word))
+    def bolded_word(self, word: str):
+        return " ".join(self._bolded_word_iter(word))
 
-    def stripped_str(self):
-        return self.text.no_niqqud().__str__()
+    def stripped_str(self):
+        return self.text.no_niqqud().__str__()
 
-    def __str__(self):
-        return self.text.__str__()
+    def __str__(self):
+        return self.text.__str__()
 
-    def __repr__(self):
-        return self.text.__repr__()
+    def __repr__(self):
+        return self.text.__repr__()
 
-    def __eq__(self, other):
-        if isinstance(other, HebrewC):
-            return self.text == other.text
-        return False
+    def __eq__(self, other):
+        if isinstance(other, HebrewC):
+            return self.text == other.text
+        return False
 
-    def __hash__(self):
-        return self.text.__hash__()
+    def __hash__(self):
+        return self.text.__hash__()
+
 
 class Loc:
-    book: str
-    chap: int
-    vers: int
+    book: str
+    chap: int
+    vers: int
 
-    def __init__(self, book: str, chap: int, vers: int):
-        self.book = book
-        self.chap = chap
-        self.vers = vers
+    def __init__(self, book: str, chap: int, vers: int):
+        self.book = book
+        self.chap = chap
+        self.vers = vers
 
-    def __repr__(self):
-        return f"<{self.book} {self.chap+1}:{self.vers+1}>"
+    def __repr__(self):
+        return f"<{self.book} {self.chap+1}:{self.vers+1}>"
 
-    def dict(self):
-        return {"book": self.book, "chap": self.chap, "vers": self.vers}
+    def dict(self):
+        return {"book": self.book, "chap": self.chap, "vers": self.vers}
 
-    @classmethod
-    def from_str(cls, loc: str):
-        book, loc = loc.split(" ")
-        chap, vers = loc.split(":")
-        return cls(book, int(chap) - 1, int(vers) - 1)
+    @classmethod
+    def from_str(cls, loc: str):
+        book, loc = loc.split(" ")
+        chap, vers = loc.split(":")
+        return cls(book, int(chap) - 1, int(vers) - 1)
+
 
 class Text:
-    name: str
-    _text: list[list[HebrewC]]
+    name: str
+    _text: list[list[HebrewC]]
 
-    def __init__(self, path: str):
+    def __init__(self, path: str):
 
-        with Path(path).open("r") as f:
-            data = json.load(f)
+        with Path(path).open("r") as f:
+            data = json.load(f)
 
-        self.name = self._get_name(data)
-        self._text = self._get_text(data)
-        self._clean_text()
+        self.name = self._get_name(data)
+        self._text = self._get_text(data)
+        self._clean_text()
 
-    def __getitem__(self, loc: str | Loc) -> HebrewC:
-        if isinstance(loc, str):
-            loc = Loc.from_str(loc)
-        if loc.book != self.name:
-            raise ValueError(f"Book {loc.book} not in text")
-        return self._text[loc.chap][loc.vers]
+    def __getitem__(self, loc: str | Loc) -> HebrewC:
+        if isinstance(loc, str):
+            loc = Loc.from_str(loc)
+        if loc.book != self.name:
+            raise ValueError(f"Book {loc.book} not in text")
+        return self._text[loc.chap][loc.vers]
 
-    def get_word(self, loc: str | Loc, word: str) -> HebrewC:
-        return HebrewC(self[loc].bolded_word(word))
+    def get_word(self, loc: str | Loc, word: str) -> HebrewC:
+        return HebrewC(self[loc].bolded_word(word))
 
-    def __iter__(self):
-        for i, chap in enumerate(self._text):
-            for j, vers in enumerate(chap):
-                yield Loc(self.name, i, j), vers
+    def __iter__(self):
+        for i, chap in enumerate(self._text):
+            for j, vers in enumerate(chap):
+                yield Loc(self.name, i, j), vers
 
-    def iterwords(self):
-        for loc, vers in self:
-            for word in vers.words():
-                yield loc, word
+    def iterwords(self):
+        for loc, vers in self:
+            for word in vers.words():
+                yield loc, word
 
-    def _clean_text(self):
-        for i, chap in enumerate(self._text):
-            for j, vers in enumerate(chap):
-                self._text[i][j] = HebrewC(vers)
+    def _clean_text(self):
+        for i, chap in enumerate(self._text):
+            for j, vers in enumerate(chap):
+                self._text[i][j] = HebrewC(vers)
 
-    @staticmethod
-    def _get_name(data: dict) -> str:
-        return data["available_versions"][0]["title"]
+    @staticmethod
+    def _get_name(data: dict) -> str:
+        return data["available_versions"][0]["title"]
 
-    @staticmethod
-    def _get_text(data: dict) -> list[list[str]]:
-        return data["versions"][0]["text"]
+    @staticmethod
+    def _get_text(data: dict) -> list[list[str]]:
+        return data["versions"][0]["text"]
+
 
 class TextCollection:
-    name: str
-    _texts: dict[str, Text]
+    name: str
+    _texts: dict[str, Text]
 
-    def __init__(self, name: str, base_path: str):
-        self.name = name
-        self._texts = {}
-        for path in Path(base_path).iterdir():
-            text = Text(path)
-            self._texts[text.name] = text
+    def __init__(self, name: str, base_path: str):
+        self.name = name
+        self._texts = {}
+        for path in Path(base_path).iterdir():
+            text = Text(path)
+            self._texts[text.name] = text
 
-    def __getitem__(self, name: str) -> Text:
-        return self._texts[name]
+    def __getitem__(self, name: str) -> Text:
+        return self._texts[name]
 
-    def __iter__(self):
-        for text in self._texts.values():
-            for loc, vers in text:
-                yield loc, vers
+    def __iter__(self):
+        for text in self._texts.values():
+            for loc, vers in text:
+                yield loc, vers
 
-    def iterwords(self):
-        for loc, vers in self:
-            for word in vers.words():
-                yield loc, word
+    def iterwords(self):
+        for loc, vers in self:
+            for word in vers.words():
+                yield loc, word
+
 
 class Lexicon:
-    def __init__(self, texts: TextCollection):
-        self.texts = texts
-        self._lex = self._make_lex()
+    def __init__(self, texts: TextCollection):
+        self.texts = texts
+        self._lex = self._make_lex()
 
-    def __getitem__(self, word: str):
-        query = HebrewC(word)
-        return self._lex[query]
+    def __getitem__(self, word: str):
+        query = HebrewC(word)
+        return self._lex[query]
 
-    def __iter__(self):
-        for word in self._lex:
-            yield word
+    def __iter__(self):
+        for word in self._lex:
+            yield word
 
-    def _make_lex(self) -> defaultdict[HebrewC, set[tuple[int, int]]]:
-        lex = defaultdict(set)
-        for loc, word in self.texts.iterwords():
-            lex[HebrewC(word)].add(loc)
-        return lex
+    def _make_lex(self) -> defaultdict[HebrewC, set[tuple[int, int]]]:
+        lex = defaultdict(set)
+        for loc, word in self.texts.iterwords():
+            lex[HebrewC(word)].add(loc)
+        return lex
+
 
 class Rhymer:
-    def __init__(self, lex: Lexicon):
-        self.lex = lex
+    def __init__(self, lex: Lexicon):
+        self.lex = lex
 
-    def rhymes(self, ending: str):
-        for word in self.lex:
-            if word.endswith(ending):
-                yield word
+    def rhymes(self, ending: str):
+        for word in self.lex:
+            if word.endswith(ending):
+                yield word
 
-    def ordered_rhymes(self, ending: str):
-        return sorted(self.rhymes(ending), key=lambda x: x.stripped_str())
+    def ordered_rhymes(self, ending: str):
+        return sorted(self.rhymes(ending), key=lambda x: x.stripped_str())
 
-    def rhymes_locs(self, ending: str):
-        rhymes = self.ordered_rhymes(ending)
-        for rhyme in rhymes:
-            yield rhyme, self.lex[rhyme]
+    def rhymes_locs(self, ending: str):
+        rhymes = self.ordered_rhymes(ending)
+        for rhyme in rhymes:
+            yield rhyme, self.lex[rhyme]
 
-    def rhymes_verses(self, ending: str):
-        for rhyme, locs in self.rhymes_locs(ending):
-            yield rhyme, [
-                (loc, self.lex.texts[loc.book].get_word(loc, rhyme))
-                for loc in set(locs)
-            ]
+    def rhymes_verses(self, ending: str):
+        for rhyme, locs in self.rhymes_locs(ending):
+            yield rhyme, [
+                (loc, self.lex.texts[loc.book].get_word(loc, rhyme))
+                for loc in set(locs)
+            ]
 
-    def rhymes_verses_json(self, ending: str):
-        verses = []
-        for rhyme, locs_verses in self.rhymes_verses(ending):
-            verses.append(
-                {
-                    "word": rhyme.text.__str__(),
-                    "verses": [
-                        {"loc": loc.dict(), "vers": vers.__str__()}
-                        for loc, vers in locs_verses
-                    ],
-                }
-            )
-        return json.dumps(verses)
+    def rhymes_verses_json(self, ending: str):
+        verses = []
+        for rhyme, locs_verses in self.rhymes_verses(ending):
+            verses.append(
+                {
+                    "word": rhyme.text.__str__(),
+                    "verses": [
+                        {"loc": loc.dict(), "vers": vers.__str__()}
+                        for loc, vers in locs_verses
+                    ],
+                }
+            )
+        return json.dumps(verses)
